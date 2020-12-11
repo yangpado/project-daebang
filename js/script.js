5257f4163d4f7283d56a8a3ad6d20c7a71ad5c86
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-11 18:02:56 +0900

    main visual img
    JS - https://splitting.js.org/

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ $(function(){
         $('.gnb .inner').removeClass('on');
     })
 })
+
+// fix header
+var scrollFix = 0;
+scrollFix = $(document).scrollTop();
+fixHeader();
+
+// 윈도우창 조절시 이벤트
+$(window).on('scroll resize', function(){
+    scrollFix = $(document).scrollTop();
+    fixHeader();
+});
+
+// 고정header함수 -> fixHeader(); 작성
+function fixHeader(){
+    if(scrollFix > 150) {
+        $('header').addClass('on');
+    } else {
+        $('header').removeClass('on');
+    }
+}
+
+// 글자애니메이션 Splitting 데모사이트 그대로 작성 따라하기
+$(function(){
+    Splitting();
+});
