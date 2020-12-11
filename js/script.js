7b157bc2c73d3ee0864250bf2f5b8aca7c0a4543
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-11 16:15:16 +0900

    JS - header mouseevent

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* gnb */
+$(function(){
+    $('.gnb>li>a').on('mouseenter focus',function(){
+        var gnbindex = $('.gnb>li>a').index($(this));
+        $('.gnb .inner').removeClass('on');
+        $('.gnb .inner:eq('+ gnbindex +')').addClass('on');
+    });
+    $('header').on('mouseleave',function(){
+        $('.gnb .inner').removeClass('on');
+    })
+})
